74c8d8f714fb018ebbc4b97f7ac1036bd001420a
Ben said Youssef <youssefbensaid827@gmail.com>
2024-04-29 17:43:41 +0100

    add fetch and delete user

--- src/Controller/AbonnementController.php
+++ src/Controller/AbonnementController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Abonnement;
+use App\Repository\AbonnementRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -19,54 +20,61 @@ class AbonnementController extends AbstractController
         ]);
     }
 
-    #[Route('/abonnements', name: 'abonnement_create', methods: ['POST'])]
-    public function create(Request $request): Response
-    {
-        $data = json_decode($request->getContent(), true);
+    // #[Route('/abonnements', name: 'abonnement_create', methods: ['POST'])]
+    // public function create(Request $request): Response
+    // {
+    //     $data = json_decode($request->getContent(), true);
 
-        $abonnement = new Abonnement();
-        $abonnement->setDateDebut(new \DateTime($data['date_debut']));
-        $abonnement->setDateFin(new \DateTime($data['date_fin']));
-        $abonnement->setPrix($data['prix']);
-        $abonnement->setPayment($data['payment']);
-        $abonnement->setIdUser($data['id_user']);
+    //     $abonnement = new Abonnement();
+    //     $abonnement->setDateDebut(new \DateTime($data['date_debut']));
+    //     $abonnement->setDateFin(new \DateTime($data['date_fin']));
+    //     $abonnement->setPrix($data['prix']);
+    //     $abonnement->setPayment($data['payment']);
+    //     $abonnement->setIdUser($data['id_user']);
+    //     $entityManager = $this->getDoctrine()->getManager();
+    //     $entityManager->persist($abonnement);
+    //     $entityManager->flush();
+
+    //     return $this->json($abonnement);
+    // }
+
+    // #[Route('/abonnements/{id}', name: 'abonnement_show', methods: ['GET'])]
+    // public function show(Abonnement $abonnement): Response
+    // {
+    //     return $this->json($abonnement);
+    // }
+
+    // #[Route('/abonnements/{id}', name: 'abonnement_update', methods: ['PUT'])]
+    // public function update(Request $request, Abonnement $abonnement): Response
+    // {
+    //     $data = json_decode($request->getContent(), true);
+
+    //     $abonnement->setDateDebut(new \DateTime($data['date_debut']));
+    //     $abonnement->setDateFin(new \DateTime($data['date_fin']));
+    //     $abonnement->setPrix($data['prix']);
+    //     $abonnement->setPayment($data['payment']);
+    //     $abonnement->setIdUser($data['id_user']);
+
+    //     $entityManager = $this->getDoctrine()->getManager();
+    //     $entityManager->flush();
+
+    //     return $this->json($abonnement);
+    // }
+
+
+    #[Route('/deleteAbn/{id}', name: 'deleteAbn', methods: ['GET' , 'POST'])]
+    public function deleteUser($id, AbonnementRepository $repo): Response {
+        $abn = $repo->findOneBy(['idAbonnement' => $id]);
+    
+        if (!$abn) {
+            throw $this->createNotFoundException('abn not found');
+        }
+    
         $entityManager = $this->getDoctrine()->getManager();
-        $entityManager->persist($abonnement);
+        $entityManager->remove($abn);
         $entityManager->flush();
-
-        return $this->json($abonnement);
+        return $this->redirectToRoute('abonnement_index');
     }
+    
 
-    #[Route('/abonnements/{id}', name: 'abonnement_show', methods: ['GET'])]
-    public function show(Abonnement $abonnement): Response
-    {
-        return $this->json($abonnement);
-    }
-
-    #[Route('/abonnements/{id}', name: 'abonnement_update', methods: ['PUT'])]
-    public function update(Request $request, Abonnement $abonnement): Response
-    {
-        $data = json_decode($request->getContent(), true);
-
-        $abonnement->setDateDebut(new \DateTime($data['date_debut']));
-        $abonnement->setDateFin(new \DateTime($data['date_fin']));
-        $abonnement->setPrix($data['prix']);
-        $abonnement->setPayment($data['payment']);
-        $abonnement->setIdUser($data['id_user']);
-
-        $entityManager = $this->getDoctrine()->getManager();
-        $entityManager->flush();
-
-        return $this->json($abonnement);
-    }
-
-    #[Route('/abonnements/{id}', name: 'abonnement_delete', methods: ['DELETE'])]
-    public function delete(Abonnement $abonnement): Response
-    {
-        $entityManager = $this->getDoctrine()->getManager();
-        $entityManager->remove($abonnement);
-        $entityManager->flush();
-
-        return $this->json(['message' => 'Abonnement deleted']);
-    }
 }
