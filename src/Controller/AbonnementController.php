9eba69de160ef0b7565066795a69cd4357aa1804
Ben said Youssef <youssefbensaid827@gmail.com>
2024-04-30 03:02:54 +0100

    add messanger

--- src/Controller/AbonnementController.php
+++ src/Controller/AbonnementController.php
@@ -20,29 +20,7 @@ class AbonnementController extends AbstractController
         ]);
     }
 
-    // #[Route('/abonnements', name: 'abonnement_create', methods: ['POST'])]
-    // public function create(Request $request): Response
-    // {
-    //     $data = json_decode($request->getContent(), true);
 
-    //     $abonnement = new Abonnement();
-    //     $abonnement->setDateDebut(new \DateTime($data['date_debut']));
-    //     $abonnement->setDateFin(new \DateTime($data['date_fin']));
-    //     $abonnement->setPrix($data['prix']);
-    //     $abonnement->setPayment($data['payment']);
-    //     $abonnement->setIdUser($data['id_user']);
-    //     $entityManager = $this->getDoctrine()->getManager();
-    //     $entityManager->persist($abonnement);
-    //     $entityManager->flush();
-
-    //     return $this->json($abonnement);
-    // }
-
-    // #[Route('/abonnements/{id}', name: 'abonnement_show', methods: ['GET'])]
-    // public function show(Abonnement $abonnement): Response
-    // {
-    //     return $this->json($abonnement);
-    // }
 
     // #[Route('/abonnements/{id}', name: 'abonnement_update', methods: ['PUT'])]
     // public function update(Request $request, Abonnement $abonnement): Response
@@ -76,5 +54,18 @@ class AbonnementController extends AbstractController
         return $this->redirectToRoute('abonnement_index');
     }
     
+    #[Route ('/fetcher/{id}', name: 'fetched', methods: ['GET' , 'POST'])]
+
+    public function fetchAbn($id, AbonnementRepository $repo): Response {
+        $abn = $repo->findOneBy(['idAbonnement' => $id]);
+
+        if(!$abn) {
+            throw $this->createNotFoundException('abn not found');
+        }
+
+        return $this->render('admin/component/abnDetails.html.twig', [
+            "abonnement" => $abn
+        ]);
+    }
 
 }
